0364c8f7b675f3d481e378b8d75999ecbc505093
Samantha Goodman <antashma@gmail.com>
2020-11-24 23:33:26 -0500

    migrate all required tables

---
+++ data/migrations/20201124232512_create-bridge-table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('projects.id');
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resources.id');
+        tbl.primary(['project_id', 'resource_id']);
+    })
+        
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('project_resources');
+};
