a4c6efe348c7dd14cdba74f2e7e800771ececb2e
Samantha Goodman <antashma@gmail.com>
2020-11-25 00:13:59 -0500

    configured server ans started routes

--- api/server.js
+++ api/server.js
@@ -1,1 +1,27 @@
 // build your server here
+const express = require('express')
+const server = express();
+
+const projectRouter = require('./project/router.js')
+
+server.use(express.json());
+server.use('/api/projects/', projectRouter);
+
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>Welcome to Sam G's Add Data Persistence Sprint!</h1>
+        <h2>Available endpoints:</h2>
+        <ul>
+            <li> add a new resource - [POST] /api/resources</li>
+            <li>retrieve all resources - [GET] /api/resources</li>
+            <li>add a new project - [POST] /api/projects</li>
+            <li>retrieve all projects - [GET] /api/projects</li>
+            <li>add a new task - [POST] /api/tasks</li>
+            <li>retrieve all tasks - [GET] /api/tasks</li>
+        </ul>
+        <p>Thanks for visiting!</p>
+    `)
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+const server = require('./api/server.js');
+
+const PORT = 3333;
+
+server.listen(PORT, () => {
+    console.log(`
+        *** Hello :) I'm listening on port ${PORT} ***
+    `)
+})
